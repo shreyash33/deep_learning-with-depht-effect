3343ac8d3ce6507636a3745ef8d1122c81cc8e64
shreyash33 <shreyashdeshpande33@gmail.com>
2020-08-14 16:35:07 +0530

    Depth Effect

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 from mtcnn.mtcnn import MTCNN
 
-model=load_model('detect_model.h5')
+model=load_model('C:/Users/SHREYASH/Downloads/detect_model.h5')
 vs=cv2.VideoCapture(0)
 det=MTCNN()
 def a():
@@ -27,6 +27,9 @@ def a():
                x,y,width,height=i['box']
                cv2.putText(frame, pred, (x,y - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,255,0), 2)
                cv2.rectangle(frame,(x,y),(x+width,y+height),(0,0,255),2)
+               croped_face=frame[y-20:y+height,x:x+width]
+               frame=cv2.blur(frame,(33,33))
+               frame[y-20:y+height,x:x+width]=croped_face
           
           cv2.imshow('face detection',frame)
           if cv2.waitKey(1) & 0xFF == ord('q'):
